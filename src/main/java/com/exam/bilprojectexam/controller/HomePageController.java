f7b7d1c70874178306aa41456e4a89b64b699900
Ayaanle A. Hassan <ayaanle2899@gmail.com>
2022-05-05 11:17:06 +0200

    first commit (project templet)

---
+++ src/main/java/com/exam/bilprojectexam/controller/HomePageController.java
@@ -0,0 +1,14 @@
+package com.exam.bilprojectexam.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+
+  @GetMapping({"/","/home","index"})
+  public String home(){
+    return "home";
+  }
+}
